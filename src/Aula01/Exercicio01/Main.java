e62903e6191f5045d7e34f0f5f7baad5e0daf61c
EmersonKersting <jrkersting1@gmail.com>
2024-09-28 14:34:18 -0300

    Correção exercicio1

--- src/Aula01/Exercicio01/Main.java
+++ src/Aula01/Exercicio01/Main.java
@@ -1,9 +1,14 @@
 package Aula01.Exercicio01;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String nome = "Emerson";
-        String sobrenome = "Kersting";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite sem nome: ");
+        String nome = sc.nextLine();
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = sc.nextLine();
 
         System.out.println("Meu nome é " + nome + " " + sobrenome);
 
